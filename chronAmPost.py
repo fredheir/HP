3a8029c3f58239d87fff5dfc70e8af5231e3ecce
Rolf Fredheim <fredheir@tcd.ie>
2014-06-20 13:21:44 +0100

    b

--- chronAmPost.py
+++ chronAmPost.py
@@ -25,6 +25,6 @@ for target in db.cd.find():
 	                                            "typos": typos,
 	                                            "caps": caps,
 	                                            "nPunct": nPunct,
-	                                            'namedEntities',pos,
+	                                            'namedEntities':pos,
 	                                            "nNames": nNames
 	                                            }})
