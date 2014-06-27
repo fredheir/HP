e7086bb8ea794a8c70508d180ecca6ab30ecfe3a
Rolf Fredheim <fredheir@tcd.ie>
2014-06-27 12:06:23 +0100

    b

--- ngrams.py
+++ ngrams.py
@@ -22,31 +22,32 @@ keep=0
 target=[u'conspir',u'plot',u'scheme',u'stratagem',u'machination',u'cabal',u'deception',u'deceit',
         u'deceive', u'ploy', u'ruse',u'dodge', u'subterfuge', u'complot',u'coup',u'colluder', u'collusion',
          u'collaborator', u'conniver', u'machinator', u'traitor',u'connive']
-while True:
-    d=next(records)
-    if d[0]==previous:
-        if keep !=0:
-            inspect.append(getEntry(d))
-    else:
-        if any (t in target for t in d[0].lower().split(' ')):
-            inspect.append(getEntry(d))
-            print d[0]
-            keep=1
-        else:
-            keep=0
-        previous=d[0]
-    counter+=1
-    if counter %1000000==0:
-        if counter <= 1000000000:
-            print str( counter/(1000000)) + (' (million)')    
-        else:
-            print str( counter/float(1000000000)) + (' (billion)')
-        print 'latest record: '+str(d)
-        #Archiving
-        if len (inspect)>0:
-            try: 
-                db.ngrams.insert(inspect,continue_on_error=True)
-            except pymongo.errors.DuplicateKeyError:
-                pass
-            inspect=[]
+for fname, url, records in readline_google_store(ngram_len=3,verbose=True):
+	print fname
+	for d in records:
+	    if d[0]==previous:
+	        if keep !=0:
+	            inspect.append(getEntry(d))
+	    else:
+	        if any (t in target for t in d[0].lower().split(' ')):
+	            inspect.append(getEntry(d))
+	            print d[0]
+	            keep=1
+	        else:
+	            keep=0
+	        previous=d[0]
+	    counter+=1
+	    if counter %1000000==0:
+	        if counter <= 1000000000:
+	            print str( counter/(1000000)) + (' (million)')    
+	        else:
+	            print str( counter/float(1000000000)) + (' (billion)')
+	        print 'latest record: '+str(d)
+	        #Archiving
+	        if len (inspect)>0:
+	            try: 
+	                db.ngrams.insert(inspect,continue_on_error=True)
+	            except pymongo.errors.DuplicateKeyError:
+	                pass
+	            inspect=[]
         
