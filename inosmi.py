d37d7343f26c4ea3df6123fd8a7843e80be2eead
Rolf Fredheim <fredheir@tcd.ie>
2014-07-04 10:51:10 +0100

    d

--- inosmi.py
+++ inosmi.py
@@ -203,7 +203,7 @@ def getAllComments(comments):
 # In[ ]:
 target= db[targetDb].aggregate( [ 
                     { '$group': { '_id':0, 'maxid': { '$max': "$_id"} } }
-                    ] )[0]['result']['maxid']
+                    ] )['result']['maxid']
 print target
 keep=[]
 
