c708980de988724752abd34d0e42bab8086e66f1
Rolf Fredheim <fredheir@tcd.ie>
2014-07-04 06:47:53 +0100

    f

--- ngrams.py
+++ ngrams.py
@@ -7,22 +7,22 @@ db = client['ngrams']
 import re
 
 def getEntry(d):
-    ngram,year,match_count,volume_count=d[0:4]
-    entry={
-           'ngram':ngram,
-           'year':year,
-           'match_count':match_count,
-           'volume_count':volume_count
-           }
-    return entry
+	ngram,year,match_count,volume_count=d[0:4]
+	entry={
+		   'ngram':ngram,
+		   'year':year,
+		   'match_count':match_count,
+		   'volume_count':volume_count
+		   }
+	return entry
 
 inspect=[]
 counter=0
 previous=""
 keep=0
 target=[u'conspir',u'scheme',u'stratagem',u'machination',u'cabal',u'deception',u'deceit',
-        u'deceive', u'ploy', u'ruse',u'dodge', u'subterfuge', u'complot',u'colluder', u'collusion',
-         u'collaborator', u'conniver', u'machinator', u'traitor',u'connive']
+		u'deceive', u'ploy', u'ruse',u'dodge', u'subterfuge', u'complot',u'colluder', u'collusion',
+		 u'collaborator', u'conniver', u'machinator', u'traitor',u'connive']
 #started=False
 for fname, url, records in readline_google_store(ngram_len=5,verbose=True):
 	words_re = re.compile(r"|\b".join(target))
@@ -31,29 +31,29 @@ for fname, url, records in readline_google_store(ngram_len=5,verbose=True):
 	#	started=True
 	#while started==True:
 	for d in records:
-		    if d[0]==previous:
-		        if keep !=0:
-		            inspect.append(getEntry(d))
-		    else:
-			    if words_re.search(d[0]):
-			            inspect.append(getEntry(d))
-		            print d[0]
-		            keep=1
-		        else:
-		            keep=0
-		        	previous=d[0]f
-		    counter+=1
-		    if counter %1000000==0:
-		        if counter <= 1000000000:
-		            print str( counter/(1000000)) + (' (million)')    
-		        else:
-		            print str( counter/float(1000000000)) + (' (billion)')
-		        print 'latest record: '+str(d)
-		        #Archiving
-		        if len (inspect)>0:
-		            try: 
-		                db.ngrams2.insert(inspect,continue_on_error=True)
-		            except pymongo.errors.DuplicateKeyError:
-		                pass
-		            inspect=[]
-	        
+			if d[0]==previous:
+				if keep !=0:
+					inspect.append(getEntry(d))
+			else:
+				if words_re.search(d[0]):
+						inspect.append(getEntry(d))
+					print d[0]
+					keep=1
+				else:
+					keep=0
+					previous=d[0]f
+			counter+=1
+			if counter %1000000==0:
+				if counter <= 1000000000:
+					print str( counter/(1000000)) + (' (million)')	
+				else:
+					print str( counter/float(1000000000)) + (' (billion)')
+				print 'latest record: '+str(d)
+				#Archiving
+				if len (inspect)>0:
+					try: 
+						db.ngrams2.insert(inspect,continue_on_error=True)
+					except pymongo.errors.DuplicateKeyError:
+						pass
+					inspect=[]
+			
